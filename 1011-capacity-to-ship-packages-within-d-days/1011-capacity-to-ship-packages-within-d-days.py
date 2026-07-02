7a4f67b6991ae6b7532a80d7912e3ceb415f87b1
GovindSharma305 <189316027+GovindSharma305@users.noreply.github.com>
2026-07-02 22:01:08 +0530

    Time: 237 ms (26.3%), Space: 23.9 MB (65.28%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def find_days(self, weights, capacity):
+        total_days = 1
+        current_load = 0
+
+        for weight in weights:
+            if current_load + weight > capacity:
+                total_days += 1
+                current_load = 0
+            current_load += weight
+
+        return total_days
+
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        low = max(weights)              
+        high = sum(weights)             
+        while low <= high:
+            mid = (low + high) // 2
+            numberOfDays = self.find_days(weights, mid)
+            if numberOfDays <= days:
+                high = mid - 1          
+            else:
+                low = mid + 1         
+        return low                      
