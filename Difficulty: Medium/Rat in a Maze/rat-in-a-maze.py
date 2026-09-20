5ac8497f8999a0a331fc948a2f94728c52e3aa78
GovindSharma305 <189316027+GovindSharma305@users.noreply.github.com>
2026-09-20 21:59:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.py
@@ -0,0 +1,46 @@
+class Solution:
+    
+    def findhelper(
+        self,
+        i: int,
+        j: int,
+        a: list[list[int]],
+        n: int,
+        ans: list[str],
+        move: str,
+        vis: list[list[int]]
+        ):
+            if i == n-1 and j == n-1:
+                ans.append(move)
+                return
+            
+            if i+1<n and not vis[i+1][j] and a[i+1][j] == 1:
+                vis[i][j] = 1
+                self.findhelper(i+1,j,a,n,ans,move+"D",vis)
+                vis[i][j] = 0
+                
+            if j-1 >= 0 and not vis[i][j-1] and a[i][j-1] == 1:
+                vis[i][j] = 1
+                self.findhelper(i,j-1,a,n,ans,move+"L",vis)
+                vis[i][j] = 0
+            
+            if j+1 < n and not vis[i][j+1] and a[i][j+1] == 1:
+                vis[i][j] = 1
+                self.findhelper(i,j+1,a,n,ans,move+"R",vis)
+                vis[i][j] = 0
+                
+            if i-1 >= 0 and not vis[i-1][j] and a[i-1][j] == 1:
+                vis[i][j] = 1
+                self.findhelper(i-1,j,a,n,ans,move+"U",vis)
+                vis[i][j] = 0
+    
+
+    
+    def ratInMaze(self, maze: list[list[int]]) -> list[str]:
+        n = len(maze)
+        ans = []
+        vis = [[0 for _ in range(n)]for _ in range(n)]
+        if maze[0][0] == 1:
+            self.findhelper(0,0,maze,n,ans,"",vis)
+        return ans
+        
